d31cad1dcb41a80bebcb55391449c71bf6cc1d00
Tim Lee <timothy.lee@intlfcstone.com>
2018-10-24 19:35:42 -0400

    Initial commits

---
+++ YouTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Timothy Lee on 10/24/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
